3fd7db2f0d5be0e6a1dbf88f361329b4c0f3be89
Francine Hahn <fran_hahn@hotmail.com>
2023-07-20 22:54:54 -0300

    feat: skeleton of the project

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <main>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+            </Head>
+
+            <h1>Olá mundo!</h1>
+        </main>
+    )
+}
