90407acacf75e0e1a27d1ad3e80436712da2e842
Francine Hahn <fran_hahn@hotmail.com>
2023-07-21 15:52:35 -0300

    chore: I added the first image and its animation

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
                 </p>
             </div>
 
-            <Image src={iphoneCover} alt="Imagem de um iphone"/>
+            <Image src={iphoneCover} alt=""/>
         </main>
     )
 }
