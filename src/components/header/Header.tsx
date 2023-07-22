770d08a721cb6ea95a558c95dde250147a82f220
Francine Hahn <fran_hahn@hotmail.com>
2023-07-22 16:12:03 -0300

    feat: I added the framer-motion to the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
 import style from "./style.module.scss";
+import { motion } from 'framer-motion';
 
 export function Header() {
     return (
-        <header className={style.header}>
+        <motion.header 
+            className={style.header}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+        >
             <h1>TechProd</h1>
             <nav>
                 <Link href="#">Home</Link>
                 <Link href="#features">O produto</Link>
                 <button>Comprar</button>
             </nav>
-        </header>
+        </motion.header>
     )
 }
